6a072494c4b86abaa29167d87e7dac54a6c66014
Naveen-1195 <mailnaveenkumar95@gmail.com>
2026-03-01 21:37:25 +0530

    add seed data for Images

--- ECommerce.Data/Data/ApplicationDbContext.cs
+++ ECommerce.Data/Data/ApplicationDbContext.cs
@@ -162,6 +162,52 @@ namespace ECommerce.DataAccess.Data
                     //ImageUrl = "\\images\\product\\3307e311-5029-45a3-9542-2f907313fefe.jpg"
                 }
                 );
+
+            modelBuilder.Entity<ProductImage>().HasData(
+
+            new ProductImage
+            {
+                id = 1,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380690/31c0cbbd-711c-40f6-8b7b-e148ffb56bf0_wxykpt.jpg",
+                ProductId = 1
+            },
+            new ProductImage
+            {
+                id = 2,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380690/2f51c4ff-0c38-4b29-aaf4-4654e25119c4_owon9f.jpg",
+                ProductId = 1
+            },
+            new ProductImage
+            {
+                id = 3,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380826/d97be00d-e501-4e2b-9076-36441d302bfc_o65d90.jpg",
+                ProductId = 2
+            },
+            new ProductImage
+            {
+                id = 4,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380868/5f652609-c116-4451-b420-f2dae144e355_qlupvd.jpg",
+                ProductId = 3
+            },
+            new ProductImage
+            {
+                id = 5,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380916/fb44daa8-12d0-4828-b90c-42ca959fd4ee_ifgg0b.jpg",
+                ProductId = 4
+            },
+            new ProductImage
+            {
+                id = 6,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380952/a9514402-1015-4c48-bf91-db8ded1bd351_p6dzuo.jpg",
+                ProductId = 5
+            },
+            new ProductImage
+            {
+                id = 7,
+                ImageUrl = "https://res.cloudinary.com/dwyvwdvqh/image/upload/v1772380996/bf56d857-5d8c-4e0e-97e6-a4d09cab2591_vivfca.jpg",
+                ProductId = 6
+            }
+        );
         }
     }
 }
